9bc4eb73096ecd97b583536f499cba7d755e0ff2
TVagneron <thomas.vagneron@gmail.com>
2017-06-08 11:51:37 +0200

    modif test déterministe, ajout test 1D déterministe

--- testthomasreduit.py
+++ testthomasreduit.py
@@ -14,7 +14,7 @@ import time
 
 logger = logging.getLogger(__name__)
 
-class TestThomasDeter(gym.Env):
+class TestThomasReduit(gym.Env):
     metadata = {
         'render.modes': ['human', 'rgb_array'],
         'video.frames_per_second' : 50
@@ -22,39 +22,33 @@ class TestThomasDeter(gym.Env):
 
     def __init__(self):
         self.tau = 0.02  # seconds between state updates
-
-	# Trees location
+	#Initial datas
+	self.vit = 1
         self.treeslocations = [3.0, 8.0];
+	#Initilisation of ending counters
+	self.count = 0
+	self.counter = 0
+	self.currentFireIndex = 0
+        # Angle at which to fail the episode
+        self.x_threshold = 1
 
-	# Initilization of counters	
-	self.count = 0     # ending of the process
-	self.currentFireIndex = 0   # tictac
-	self.timecount = 0 # 0 -> 1 -> ... -> 50 -> 0
-	self.firetrees = 0 # number of trees on fire
-
-	# Actions: 0: fire 1 / 1: fire 2 / 2: nothing
-        self.action_space = spaces.Discrete(3)
-
-	# TODO necessary?
-	self.worldWidth = 4.8
-	self.worldWidth2 = 2*24*math.pi/360
-        
-	# Observation space definition (TODO redefine with actual observation space)
+        # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds$
         high_value = [1]
         low_value = [0]
 	#self.observation_space = spaces.Box(-high, high) # TODO TODO TODO WARNING: between -0 and 0!!! 
-	upper_space = np.array(high_value*len(self.treeslocations))
-	lower_space = np.array(low_value*len(self.treeslocations))
+	upper_space = np.array([self.x_threshold]+high_value*len(self.treeslocations))
+	lower_space = np.array([-self.x_threshold]+low_value*len(self.treeslocations))
 	self.observation_space = spaces.Box(lower_space, upper_space)
-# TODO
-# self.observation_space = spaces.Discrete(2)
-# loop from 1 to nbtree
-# self.observation_space = Tuple(spaces.Discrete(2), self.observation_space )
 
-	# seed, init values, viewer reset
+
+        self.action_space = spaces.Discrete(3)
+
+
         self._seed()
         self.reset()
         self.viewer = None
+
+        self.steps_beyond_done = None
 
         # Just need to initialize the relevant attributes
         self._configure()
@@ -66,74 +60,84 @@ class TestThomasDeter(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    # =======================================================================================================================================================
-    # ============ STEP =====================================================================================================================================
-    # =======================================================================================================================================================
     def _step(self, action):
-
 	screen_width = 600
         screen_height = 400
-        world_width = self.worldWidth
+        reward = 0.0
+
+        world_width = self.x_threshold*2
         scale = screen_width/world_width
 
         assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
         state = self.state
-
-	# parse state (fire states)
+        #print("bla")
+        #print(state)
+        #x, x_dot, theta, theta_dot, v1, v2, v3, v4 = state # TODO TODO TODO access data from a tuple
+        x = state[0]
         treesStatesList = []
-	for i in range(len(self.treeslocations)):
-           treesStatesList.append(state[i])
+        for i in range(len(self.treeslocations)):
+            treesStatesList.append(state[1+i])
 	treesStates = ()
-
-	# fire starter	
+        #print(treesStates)
+        
+        # fire starter	
         if (self.timecount > 0):
         	self.timecount += 1        
         if (self.timecount == 50):
         	self.timecount = 0
-        	
-	# ONLY ONE FIRE AT EACH STEP
-        if (treesStatesList[self.currentFireIndex]==0) and (self.timecount == 0):
+	
+	#state of trees
+	if ((treesStatesList[self.currentFireIndex]==0) and (self.timecount == 0) and self.firetrees == 0):
             	treesStatesList[self.currentFireIndex] = 1 # ON FIRE!!
                 self.firetrees = 1
                 self.currentFireIndex += 1
                 if (self.currentFireIndex == len(self.treeslocations)):
                 	self.currentFireIndex = 0
+       
+	#movement
+	vitesse = 0
+	if (action == 1 and x<self.x_threshold):
+		vitesse = self.vit
+	elif (action == 0 and x > -self.x_threshold):
+		vitesse = -self.vit
+	#vitesse = 1
+        x  = x + self.tau * vitesse
+	#action of extinguishing the fire
+	for i in range(len(self.treeslocations)):
+		if (((self.treeslocations[i]-5)/5-0.05<x<(self.treeslocations[i]-5)/5+0.05) and self.state[1+i]==1 and action == 2):
+			treesStatesList[i] = 0
+			reward = 1.0
+			self.timecount += 1			
 
-        for i in range(len(self.treeslocations)):
-        	if (treesStatesList[i] == 1 and action == i):
-        		treesStatesList[i] = 0 	# EXTIGUISHED BY ROBOT
-        		reward = 1.0 		# REWARD++
-        		self.firetrees = 0
-        		self.timecount += 1
-        	else :
-        		reward = 0.0 		# NO REWARD
 	for i in range(len(self.treeslocations)):
                	treesStates += (treesStatesList[i],)
-	self.state = treesStates 
-	
+
+        #print(treesStates)
+	#print(self.state[0]*scale/world_width+0.5)
+        self.state = (x,) + treesStates 
+
+
+        #time.sleep(0.5)
 	self.count += 1
 	done = False
 
-	# END OF THE EPISODE
+	#ending conditions
 	if (self.count == 20000):
 		done = True
-        
+        print(self.state)
 	return np.array(self.state), reward, done, {}
 
     def _reset(self):
-	treesStates = ()
+        treesStates = ()
         for i in range(len(self.treeslocations)):
 	     treesStates += (0,)
-        self.state = treesStates
+        self.state = np.concatenate((self.np_random.uniform(low=-0.05, high=0.05, size=(1,)),treesStates), axis = 0)
 	self.currentFireIndex = 0
 	self.count = 0
 	self.timecount = 0
+	self.firetrees = 0
+	print(self.state)
         return np.array(self.state)
-
-
-    # =======================================================================================================================================================
-    # ============ VIEWER ===================================================================================================================================
-    # =======================================================================================================================================================
     def _render(self, mode='human', close=False):
         if close:
             if self.viewer is not None:
@@ -144,27 +148,47 @@ class TestThomasDeter(gym.Env):
         screen_width = 600
         screen_height = 400
 
-        world_width = self.worldWidth
+        world_width = self.x_threshold*2
         scale = screen_width/world_width
-        carty = 100 # y-position
+        carty = 100 # TOP OF CART
+        polewidth = 10.0
+        polelen = scale * 1.0
+        cartwidth = 50.0
+        cartheight = 30.0
 	
         if self.viewer is None:
             from gym.envs.classic_control import rendering
             self.viewer = rendering.Viewer(screen_width, screen_height, display=self.display)
+            l,r,t,b = -cartwidth/2, cartwidth/2, cartheight/2, -cartheight/2
+
+	    self.chose = []
+            cart = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
+	    self.chose.append(cart)
+            self.carttrans = rendering.Transform()
+            cart.add_attr(self.carttrans)
+            self.viewer.add_geom(self.chose[0])
+            self.track = rendering.Line((0,carty), (screen_width,carty))
+            self.track.set_color(0,0,0)
+            self.viewer.add_geom(self.track)
             self.truc = []
             cpt = 0
-		# trees view definition
             for tree in self.treeslocations:
                 self.truc.append(rendering.FilledPolygon([(tree*screen_width/10-10,carty-10), (tree*screen_width/10+10,carty-10), (tree*screen_width/10+10,carty+10), (tree*screen_width/10-10,carty+10)]))
                 self.truc[cpt].set_color(0,1,0)
+                #self.treeColor = rendering.Transform()
+                #truc.add_attr(self.treeColor)
                 self.viewer.add_geom(self.truc[cpt])
                 cpt+=1
+                
+                
 
-        x = self.state
-	# tree color management
+        pos = self.state
+        cartx = pos[0]*scale+screen_width/2.0 # MIDDLE OF CART
+        self.carttrans.set_translation(cartx, carty)
         for i in range(len(self.treeslocations)):
-            if(x[i]==1):
+            if(pos[i+1]==1):
                 self.truc[i].set_color(1,0,0)
             else:
 		self.truc[i].set_color(0,1,0)
+
         return self.viewer.render(return_rgb_array = mode=='rgb_array')
